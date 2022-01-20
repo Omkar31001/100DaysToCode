f4b36e9f69f78f2bf7fc74c55746654bf669ea1d
Omkar Dave <62291426+Omkar31001@users.noreply.github.com>
2022-01-20 12:34:28 +0300

    Time: 45 ms (25.59%), Space: 51.4 MB (41.39%) - LeetHub

---
+++ 1476-subrectangle-queries/1476-subrectangle-queries.java
@@ -0,0 +1,31 @@
+class SubrectangleQueries {
+      int[][] res;
+    public SubrectangleQueries(int[][] rectangle) {
+        res =rectangle;
+    }
+    
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        if(row1==row2){
+            for(int i=col1;i<=col2;i++){
+                res[row1][i] = newValue;
+            }
+        }else{
+                for(int i=row1;i<=row2;i++){
+                    for(int j=col1;j<=col2;j++){
+                        res[i][j] = newValue;
+                    }
+                }
+            }
+    }
+    
+    public int getValue(int row, int col) {
+        return res[row][col];
+    }
+}
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
+ * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
+ * int param_2 = obj.getValue(row,col);
+ */
