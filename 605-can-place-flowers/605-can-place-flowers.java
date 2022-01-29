c35642522d857840d4ea47ee5c0d4adce49163b4
Omkar Dave <62291426+Omkar31001@users.noreply.github.com>
2022-01-29 13:37:44 +0300

    Time: 1 ms (95.40%), Space: 42.9 MB (68.24%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flo, int n) {
+      int k = flo.length;
+        int count=0;
+      
+        for(int i=0;i<k;i++){
+            int prev=(i==0 || flo[i-1]==0)?0:1;
+            int nxt=(i==k-1 || flo[i+1]==0)?0:1;
+            if(prev==0 && nxt==0 && flo[i]==0){
+                flo[i]=1;
+                count++;
+            }
+        }
+       return count>=n;
+    }
+}
